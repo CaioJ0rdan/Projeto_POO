6709898a7662a4dadff3837cd55c43e5716d8c5a
torreslucs23 <luquinhastorres231002@gmail.com>
2022-11-29 14:44:58 -0300

    added main

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "Jogador.h"
+#include "Date.h"
+#include "Goleiro.h"
+#include "Atacante.h"
+#include "Partida.h"
+#include "Zagueiro.h"
+#include "time.h"
+
+
+using namespace std;
+
+
+
+
+int main(){
+
+
+    return 0;
+}
